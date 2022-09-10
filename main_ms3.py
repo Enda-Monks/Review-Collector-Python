35cdd9b9985b11f98605a8f58b9875a1c0367855
Enda-Monks <enda.monks@gmail.com>
2022-09-10 18:49:58 -0400

    comments and env

--- main_ms3.py
+++ main_ms3.py
@@ -3,6 +3,19 @@
 # Milestone 2 - Instantiate connect to elastic search collection via .env file
 #             - Prompt user to send data to collection
 #             - Load Album data from locally stored JSON file   (loop starts here)
-#             - *Try* to send (POST?) each collection of json objects to database
+#             - *Try* to POST each collection of json objects to database, *catch* errors
 #             - Display success message                         (loop ends here)
 # -------------------------------------------------------------------------------
+
+# Tasker Notes:----------------------------------------------------------------------------------
+# secret values store in .env get "client = elasticbud.client.get_elasticsearch_client()" to work
+# use same username and password to log in to browser based visualization app that sits on top of 
+# Elasticsearch (called Kibana), here: https://kibana.ialcloud.xyz
+
+# Random ElasticBud Notes:-----------------------------------------------------------------------
+# index_to_elasticsearch func from elasticsearch library https://github.com/z-tasker/elasticbud/blob/bd539e2df4671a762b0f8fd7dce2d922e990174b/tests/test_integration_flow.py#L53
+# get_elasticsearch_client func <...client.py>
+# line 133 https://github.com/z-tasker/elasticbud/blob/bd539e2df4671a762b0f8fd7dce2d922e990174b/elasticbud/elasticsearch.py
+# all https://github.com/z-tasker/elasticbud/blob/bd539e2df4671a762b0f8fd7dce2d922e990174b/elasticbud/client.py 
+# -----------------------------------------------------------------------------------------------
+
